6620088485b2b4b324490c696efdb8ee265f7cfb
Sam Warnick <sam@warnick.me>
2024-11-16 13:48:30 -0500

    Update schema for cards

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,14 +1,24 @@
 import { sql, type InferSelectModel } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 import { createInsertSchema } from 'drizzle-zod';
+import { EEVEELUTIONS } from '../utils/eeveelutions';
 
-export const messages = sqliteTable('messages', {
-	id: integer('id').primaryKey(),
+export const cards = sqliteTable('cards', {
+	id: text('id').primaryKey(),
 	createdAt: text('created_at')
 		.notNull()
 		.default(sql`(CURRENT_TIMESTAMP)`),
-	message: text('message').notNull(),
+	cardName: text('cardName').notNull(),
+	pokemonName: text("pokemonName", {enum: EEVEELUTIONS}).notNull(),
+	pokedexNumber: integer('pokedexNumber').notNull(),
+	imageSmall: text('imageSmall').notNull(),
+	imageLarge: text('imageLarge').notNull(),
+	setId: text('setId').notNull(),
+	setName: text('setName').notNull(),
+	setReleaseDate: text('setReleaseDate').notNull(),
+	marketPrice: integer('marketPrice').notNull(),
+	owned: integer('owned', {mode: "boolean"}).notNull().default(false)
 });
 
-export type Message = InferSelectModel<typeof messages>;
-export const insertMessageSchema = createInsertSchema(messages);
+export type Card = InferSelectModel<typeof cards>;
+export const insertCardSchema = createInsertSchema(cards);
