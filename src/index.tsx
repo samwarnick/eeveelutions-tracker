db15d5ad02c76aff7e57be9f0b39767c2190dd01
Sam Warnick <sam@warnick.me>
2024-11-16 15:57:17 -0500

    Add tcg-card component

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 import { serveStatic } from 'hono/bun';
 import { logger } from 'hono/logger';
-import { Hello } from './components/hello';
 import { db } from './db/db';
-import { insertMessageSchema, messages } from './db/schema';
 import { Layout } from './layout';
+import { cards } from './db/schema';
+import { EEVEELUTIONS } from './utils/eeveelutions';
 
 const app = new Hono();
 
@@ -20,33 +20,24 @@ app.use(
 );
 
 app.get('/', async (c) => {
-	const allMessages = await db.select().from(messages);
+	const allCards = await db.select().from(cards);
 	return c.html(
 		<Layout>
-			<Hello />
-			<ul id="messages">
-				{allMessages.map((m) => (
-					<li>{m.message}</li>
-				))}
-			</ul>
-			<form hx-post="/" hx-swap="beforeend" hx-target="#messages">
-				<fieldset role="group">
-					<input
-						name="message"
-						type="message"
-						placeholder="Enter your message"
-					/>
-					<input type="submit" value="Submit" />
-				</fieldset>
-			</form>
+			<select name="select" aria-label="Select" required>
+				<option selected>All</option>
+				{EEVEELUTIONS.map((name) => (<option value={name}>{name}</option>))}
+			</select>
+			<tcg-card-wrapper>
+				{allCards.map((card) => (<article>
+					<tcg-card src={card.imageLarge} alt={card.cardName}></tcg-card>
+					<div class="content">
+						<h2>{card.cardName}</h2>
+						<p>{card.setName} ({card.setReleaseDate})</p>
+					</div>
+				</article>))}
+			</tcg-card-wrapper>
 		</Layout>,
 	);
-});
-
-app.post('/', zValidator('form', insertMessageSchema), async (c) => {
-	const newMessage = c.req.valid('form');
-	await db.insert(messages).values({ message: newMessage.message });
-	return c.html(<li>{newMessage.message}</li>);
 });
 
 export default app;
--- src/layout.tsx
+++ src/layout.tsx
@@ -6,12 +6,12 @@ export const Layout: FC = (props) => {
 			<head>
 				<meta charset="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<title>The Perfect Stack</title>
+				<title>Eeeveelutions Tracker</title>
 				<link rel="stylesheet" href="/assets/picocss@2.0.6.min.css" />
 				<link rel="stylesheet" href="/assets/styles.css" />
 				<script src="/assets/htmx@2.0.1.min.js" />
-				<script src="/assets/alpinejs@3.14.1.min.js" />
-				<script src="/assets/tcg-card.js" />
+				<script defer src="/assets/alpinejs@3.14.1.min.js" />
+				<script role="module" src="/assets/tcg-card.js" />
 			</head>
 			<body>
 				<main class="container">{props.children}</main>
